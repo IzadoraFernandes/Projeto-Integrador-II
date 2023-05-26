73f89bb7e1b058c633271f149e83e9d617b82ebc
IzadoraFernandes <izadorasilvams@gmail.com>
2023-05-26 08:39:09 -0300

    trabalhando com emprestimos de livros

--- livro/models.py
+++ livro/models.py
@@ -1,19 +1,52 @@
 from django.db import models
 from datetime import date
+from usuarios.models import Usuario
 
-class Emprestimo(models.Model):
-    pass
+class Categoria(models.Model):
+    nome_categoria = models.CharField(max_length=50)
+    descricao_categoria = models.TextField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
 
+    
+    def __str__(self) -> str:
+        return self.nome_categoria
+    
 class Livros(models.Model):
-    nome = models.CharField(max_length = 100)
+    nome_livro = models.CharField(max_length = 100)
     autor = models.CharField(max_length = 30)
     co_autor = models.CharField(max_length= 30, blank= True, null=True)
     data_cadastro = models.DateField(default = date.today)
+    quantidade = models.IntegerField()
     emprestado = models.BooleanField(default=False)
-    nome_emprestado = models.CharField(max_length= 30, blank= True, null=True)
-    data_emprestimo = models.DateTimeField(blank= True, null=True)
-    data_devolucao = models.DateTimeField(blank= True, null=True)
-    tempo_duracao = models.DateField(blank= True, null=True)
+    descricao_livro = models.TextField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    editora = models.CharField(max_length = 100)
+    data_lancamento = models.DateField(default = date.today)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING) # se a categoria for apagada ele simplesmente ignora.
 
+    
     class Meta:
         verbose_name = 'Livro' #para retirar os dois s.
+        
+    def __str__(self):
+        return self.nome_livro
+    
+class Emprestimo(models.Model):
+    
+    choices = (
+        ('P', 'Péssimo'),
+        ('R', 'Ruim'),
+        ('B', 'Bom'),
+        ('O', 'Ótimo')
+    )
+    
+    nome_emprestado_usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING, blank = True, null = True, related_name='emprestimo_usuario') #registro de usuários cadastrados no sistema.
+    nome_emprestado_anonimo = models.CharField(max_length = 30, blank = True, null = True) #para usuários não cadastrados no sistema
+    data_devolucao = models.DateTimeField(blank= True, null=True)
+    tempo_duracao = models.DateField(blank= True, null=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    livro = models.ForeignKey(Livros, on_delete=models.DO_NOTHING)
+    avaliacao = models.CharField(max_length=1, choices=choices, null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return f"{self.nome_emprestado} | {self.livro}"
--- livro/views.py
+++ livro/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from usuarios.models import Usuario
+from .models import Livros, Categoria
 
 def home(request):
     if request.session.get('usuario'): 
-        
-        return HttpResponse('Pandora')
+        usuario = Usuario.objects.get(id = request.session['usuario'])
+        livros = Livros.objects.all()  #filter(usuario = usuario) só exibir os livros dele mesmo
+        total_livros = livros.count()
+        return render(request, 'home.html', {'livros':livros,})
+    else:
+        return redirect('/auth/login/?status=2')
+
+def ver_livros(request, id):
+    if request.session.get('usuario'):
+        livro = Livros.objects.get(id = id)
+        if request.session.get('usuario') == livro.usuario.id:
+            usuario = Usuario.objects.get(id = request.session['usuario'])
+            categoria_livro = Categoria.objects.filter(usuario = request.session.get('usuario')) # para só exibir as categorias que o proprio usuário criou e não de todos.
+            print(categoria_livro)
+            return render(request, 'ver_livro.html', {'livro': livro,})
+        else:
+            return HttpResponse('Esse livro não é seu')
     else:
         return redirect('/auth/login/?status=2')
